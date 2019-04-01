961902e966551df226b2e2be6573dd3a97eb7339
Jason Kim <jjkim2320@gmail.com>
2019-03-31 22:37:19 -0700

    added cors allow headers

--- scores.php
+++ scores.php
@@ -1,7 +1,8 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, userId, date");
+	
     include_once './config/database.php';
     include_once './objects/score.php';
 	include_once './objects/helper.php';
